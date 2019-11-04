52583e5ef9a7fe13f5cc336c2ac9acd9eea51157
QingYang Dong <54829589+Malloy-QD@users.noreply.github.com>
2019-11-04 17:47:16 -0600

    Add files via upload

--- DeletePost.php
+++ DeletePost.php
@@ -1,22 +1,16 @@
 <?php
-$post_id=$_POST['del'];
-$final= implode(",",$post_id);
 $mysqli =new mysqli("mysql.eecs.ku.edu", "q527d476", "aafei4ah", "q527d476");
-if(count($post_id)==0)
-{
-    echo "Choose which one to delete";
-}
-else {
+
+$select=$_POST['select'];
     if ($mysqli->connect_errno) {
         die("Connect failed: %s\n" . $mysqli->connect_error);
     }
-    for($i=0;$i<count($post_id);$i++){
-        $current = $post_id[$i];
-        $query = "DELETE FROM Posts WHERE post_id ='$current'";
+    foreach($select as $delete){
+        $query = "DELETE FROM Posts WHERE post_id ='$delete'";
         if($result = $mysqli->query($query)){
-            echo "Successfully deleted the post:".$current."<br>";
+            echo "Successfully deleted the post:".$delete."<br>";
         }
     }
-}
+
 $mysqli->close();
 ?>
--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -2,12 +2,12 @@
 
 $mysqli =new mysqli("mysql.eecs.ku.edu", "q527d476", "aafei4ah", "q527d476");
 $user_id = $_POST['select'];
-$query="SELECT * FROM Posts WHERE author_id= '$user_id'";
+$post="SELECT content FROM Posts WHERE author_id= '$user_id'";
 if ($mysqli->connect_errno)
 {
     die("Connect failed: %s\n". $mysqli->connect_error);
 }
-if($result = $mysqli->query($query)){
+if($result = $mysqli->query($post)){
     while($row = $result->fetch_assoc()){
         echo "<table>";
         echo "POST:";
