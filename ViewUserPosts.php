6e7ee4222aaf0effdd37f26a2b488b6d278556f4
QingYang Dong <54829589+Malloy-QD@users.noreply.github.com>
2019-11-05 09:45:56 -0600

    Add files via upload

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -2,19 +2,26 @@
 
 $mysqli =new mysqli("mysql.eecs.ku.edu", "q527d476", "aafei4ah", "q527d476");
 $user_id = $_POST['select'];
-$post="SELECT content FROM Posts WHERE author_id= '$user_id'";
+$post="SELECT content FROM Posts WHERE author_id= $user_id";
 if ($mysqli->connect_errno)
 {
     die("Connect failed: %s\n". $mysqli->connect_error);
 }
-if($result = $mysqli->query($post)){
-    while($row = $result->fetch_assoc()){
-        echo "<table>";
-        echo "POST:";
-        echo "<tr><td>"."@".$row["post_id"].":".$row["content"]."</td></tr>";
-        echo "</table>";
+echo "<table>";
+echo "POST:";
+if($result = $mysqli->query($post))
+{
+    while($row = $result->fetch_assoc())
+    {
+      echo "<tr>";
+      echo "<td>".$row['content']."</td>";
+      echo "</tr>";
     }
+
+    $result->free();
 }
-$result->free();
+
+        echo "</table>";
+
 $mysqli->close();
 ?>
